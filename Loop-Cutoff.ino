40d22518f8494185db13330a46a8b15840720103
dorkmo <dorkmo@daum.net>
2018-05-06 16:05:49 -0500

    Update Loop-Cutoff.ino

--- Loop-Cutoff.ino
+++ Loop-Cutoff.ino
@@ -1,1 +1,7 @@
 //periodically checks for radio message. If recieve kill signal, flip relay module. Send Confirmation that relay has been engaged.
+
+int p2 = 2; //define "read" Pin to confirm relay has been flipped
+
+int r1 = 1; //define "write" pins to control Relay module
+
+int s2 = digitalRead(2);  //to handle data of current State of the relay
