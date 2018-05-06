6d423b05b40f6f53a769aa03d4772e9d216f1a2d
dorkmo <dorkmo@daum.net>
2018-05-06 15:34:53 -0500

    Update Loop-Level.ino

--- Loop-Level.ino
+++ Loop-Level.ino
@@ -1,1 +1,1 @@
-//used to monitor level height switch and send cut off signal to remote kill switch box
+//used to monitor level height switch and send cut off signal to remote kill switch box. When recieve confirmation of cutoff, end sending message until level switch is reset.
