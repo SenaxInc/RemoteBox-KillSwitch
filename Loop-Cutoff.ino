b73c5e1fb9d29fda5ce1598fe99f113568575b13
dorkmo <dorkmo@daum.net>
2018-05-06 16:07:03 -0500

    Update Loop-Cutoff.ino

--- Loop-Cutoff.ino
+++ Loop-Cutoff.ino
@@ -5,3 +5,5 @@ int p2 = 2; //define "read" Pin to confirm relay has been flipped
 int r1 = 1; //define "write" pins to control Relay module
 
 int s2 = digitalRead(2);  //to handle data of current State of the relay
+
+int b2;  //to temporarily remember the last Broadcast data of the realy
