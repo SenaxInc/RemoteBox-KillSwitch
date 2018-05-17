eb6260f76b8d02b4c24f3b32ae5c8db29d0f2f1a
dorkmo <dorkmo@daum.net>
2018-05-17 14:02:54 -0500

    Update LoopSimple-Level.ino

--- LoopSimple-Level.ino
+++ LoopSimple-Level.ino
@@ -10,9 +10,13 @@ int b10;  //to temporarily remember the last Broadcast data for the switch
 
 int s10 = digitalRead(p10);  //to handle data of current State of a switch
 
-int ST = 5; //minutes of radio inactivity before XBEE goes into sleep mode
+int bedtime = 0;
 
-int SP = 25; //minutes XBEE will sleep before waking the radio back up
+int st = 5; //minutes of radio inactivity before XBEE goes into sleep mode
+
+int sp = 25; //minutes XBEE will sleep before waking the radio back up
+
+int tick8=0;
 
 int messageState = HIGH;         // the current state of the output pin
 int lvlState;             // the current reading from the input pin
@@ -37,57 +41,8 @@ while (bedtime=0){
 Serial.begin(9600);      //START COMMUNICATING WITH XBEE  
 delay(5000);                              //wait 5 seconds  
     
-debounceReadPin();
-delay(5000);           //wait 5 seconds
-  
-  if (s10 == LOW) {       //And If the current state of the switch is "low", AKA the switch is tripped...      
-    Serial.print('K');   //Send the Kill Signal
-    delay(5000);     
-  } else {               //If the switch was not "low"...
-    Serial.print('A');   //Send Alive Signal
-    delay(5000);     
-  }
-  
-while (Serial.available()>0){             //if message available...
-    
-char RXbyte = char(Serial.read()); //read message...
-  
-if (RXbyte == 'k') { //if message is "k"...
-    Serial.print('Z'); 
-}
-if (RXbyte == 'a') { //if message is "a"...
-    Serial.print('Z'); 
-}  
- 
-if (RXbyte == 'z') {
-    Serial.end(9600);                    //end communication with xbee
-    bedtime=1;                           //initiate bedtime next loop
-}
+////// DEBOUNCE
 
-delay(5000);
-  
-} //end serial available while
-
-}
-  
-}
-
-} //end bedtime=0
-
-while (bedtime=1){
-
- delay(8000);                             //wait 8 seconds
- tick8++;                                 //add 1 to "tick8"
-  
-if (tick8>180){                          //after 24 minutes...
-    bedtime=0;                           //end bedtime
-}                                        //end tick8 if  
-  
-} //end bedtime=1 while
-
-} //end main loop  
-
-void debounceReadPin() {
   // read the state of the switch into a local variable:
   int reading = digitalRead(p10);
 
@@ -121,4 +76,51 @@ void debounceReadPin() {
 
   // save the reading. Next time through the loop, it'll be the lastButtonState:
   lastlvlState = reading;
+
+////// END DEBOUNCE
+
+delay(5000);           //wait 5 seconds
+  
+  if (s10 == LOW) {       //And If the current state of the switch is "low", AKA the switch is tripped...      
+    Serial.print('K');   //Send the Kill Signal
+    delay(5000);     
+  } else {               //If the switch was not "low"...
+    Serial.print('A');   //Send Alive Signal
+    delay(5000);     
+  }
+  
+while (Serial.available()>0){             //if message available...
+    
+char RXbyte = char(Serial.read()); //read message...
+  
+if (RXbyte == 'k') { //if message is "k"...
+    Serial.print('Z'); 
 }
+if (RXbyte == 'a') { //if message is "a"...
+    Serial.print('Z'); 
+}  
+ 
+if (RXbyte == 'z') {
+                                         //end communication with xbee
+    bedtime=1;                           //initiate bedtime next loop
+}
+
+delay(5000);
+  
+} //end serial available while
+
+} //end bedtime=0
+
+while (bedtime=1){
+
+ delay(8000);                             //wait 8 seconds
+ tick8++;                                 //add 1 to "tick8"
+  
+if (tick8>180){                          //after 24 minutes...
+    bedtime=0;                           //end bedtime
+}                                        //end tick8 if  
+  
+} //end bedtime=1 while
+
+} //end main loop  
+
